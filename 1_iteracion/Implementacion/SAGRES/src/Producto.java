0a06a60f3bd7c6a06ee0884cb141a7141f8ea6fa
jose2dr <jose2dr@gmail.com>
2010-03-28 14:16:04 +0000

    Primera version de la clase. Falta comentar con el grupo de diseño un par de aspectos relativos al metodo para colocar imagenes.

---
+++ 1_iteracion/Implementacion/SAGRES/src/Producto.java
@@ -0,0 +1,69 @@
+
+import javax.swing.ImageIcon;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jose David Dionisio Ruiz
+ */
+public class Producto {
+    private int cantidad;
+    private int codPro;
+    private int maximo;
+    private int minimo;
+    private String nombre;
+    private ImageIcon imagen;
+
+    public Producto(ImageIcon imagen, String nombre,int minimo, int maximo, int cantidad, int codPro) {
+        this.cantidad = cantidad;
+        //this.codPro = generarCodigoProducto();
+        this.maximo = maximo;
+        this.minimo = minimo;
+        this.nombre = nombre;
+        this.imagen = imagen;
+    }
+
+    public int getCantidad(){
+        return this.cantidad;
+    }
+
+    public int getMaximo(){
+        return this.maximo;
+    }
+
+    public int getMinimo(){
+        return this.minimo;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void restarCantidad(int resta){
+        this.cantidad -= resta;
+    }
+
+    public void setFoto(ImageIcon foto){
+        this.imagen = foto;
+    }
+
+    public void setMaximo(int maximo){
+        this.maximo = maximo;
+    }
+    
+    public void setMinimo(int minimo){
+        this.minimo = minimo;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void sumarCantidad(int suma){
+        this.cantidad += suma;
+    }
+}
