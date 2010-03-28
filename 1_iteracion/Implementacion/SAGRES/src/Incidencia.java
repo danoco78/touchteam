41c82a063d96fdbf937774886d44e3a3782b127b
danigrmartinez <danigrmartinez@841c7c5c-26d4-11df-9191-49dca8b91aeb>
2010-03-28 14:19:59 +0000

    Añadidas clases Incidencia, PedidoProveedor. Modificado Documento_implementacion_v0.1.odt

---
+++ 1_iteracion/Implementacion/SAGRES/src/Incidencia.java
@@ -0,0 +1,60 @@
+
+import java.util.Date;
+
+
+/**
+ *
+ * @author Daniel
+ */
+public class Incidencia {
+
+    private int cantidadAfectada;
+    private int codigo;
+    private String descripcion;
+    private Date fecha;
+    public Producto m_Producto;
+
+    @Override
+    public void finalize() throws Throwable {
+    }
+
+    /**
+     * Crea un objeto Incidencia
+     * @param codigo Representación unica para objeto
+     * @param cantidad Indica el numero de productos afectados
+     * @param descripcion Descripción del motivo por el cual fueron desechados.
+     * @param fecha Fecha de incidencia.
+     */
+    public Incidencia(int codigo, int cantidad, String descripcion, Date fecha) {
+        this.codigo=codigo;
+        this.cantidadAfectada=cantidad;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+    }
+
+    /**
+     * Modifica la cantidad afectada.
+     * @param cantidad
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidadAfectada=cantidad;
+    }
+
+    /**
+     * Modifica la descripción de las causas
+     * @param descripcion
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * Modifica la fecha de la incidencia.
+     * @param fecha
+     */
+    public void setFechaIncidencia(Date fecha) {
+        this.fecha = fecha;
+    }
+
+
+}
