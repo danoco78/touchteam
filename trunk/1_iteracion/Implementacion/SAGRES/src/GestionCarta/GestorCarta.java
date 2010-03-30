0c2097526191727fbeb62a624ac38e0c30fb71bc
carlos.salas@gmail.com <carlos.salas@gmail.com@841c7c5c-26d4-11df-9191-49dca8b91aeb>
2010-03-30 12:16:42 +0000

    Añadido cuerpo de varios métodos de la clase GestorCarta en versión inicial. Modificados varios parámetros de acuerdo a la nueva versión del documento de diseño.

--- trunk/1_iteracion/Implementacion/SAGRES/src/GestionCarta/GestorCarta.java
+++ trunk/1_iteracion/Implementacion/SAGRES/src/GestionCarta/GestorCarta.java
@@ -5,6 +5,7 @@ import GestionStock.GestionProductos.Bebida;
 import GestionStock.GestionProductos.Ingrediente;
 import GestionStock.GestionProductos.Producto;
 import java.util.ArrayList;
+import java.util.Iterator;
 import javax.swing.ImageIcon;
 
 
@@ -44,7 +45,28 @@ public class GestorCarta implements IPreparaCarta, ICarta {
         return new ArrayList<Elemento>();
     }
 
-    public void eliminaElementoCarta(int codigoElemento) {}
+    public void eliminaElementoCarta(int codigoElemento) {
+
+        boolean encontrado = false;
+        Elemento ele = null;
+        Iterator it = elemento.iterator();
+        // Mientras haya elementos y no encontremos el que buscamos
+        while (it.hasNext() && !encontrado) {
+            ele = (Elemento)it.next();
+            /* Comprobamos si el código del elemento extraido se corresponde
+             * con el código recibido como parámetro. Si es así salimos del bucle
+             */
+            if (ele.getCodigoElemento() == codigoElemento)
+                encontrado = true;
+        }
+        if (encontrado == true)
+            elemento.remove(ele);
+        /* else
+             Lanzar excepción no existe elemento */
+
+        // Eliminar de la BD
+
+    }
 
     public int generaCodigoElemento() {
         return 1;
@@ -60,16 +82,36 @@ public class GestorCarta implements IPreparaCarta, ICarta {
     }
 
     public void modificaElementoPlato(int codigoElemento, String nombre,
-            String descripcion, ImageIcon foto, float tiempoPreparacion,
+            String descripcion, ImageIcon foto, int tiempoPreparacion,
             float precio, int divisionesMaximas) {}
 
     public void nuevoElementoBebida(ArrayList<Bebida> listaBebidas,
-            String seccion, String nombre, String descripcion, float precio,
-            ImageIcon foto, int divisionesMaximas) {}
+            Seccion seccion, String nombre, String descripcion, float precio,
+            ImageIcon foto, int divisionesMaximas) {
+
+        // Comprobación que el elemento es correcto
+
+            int codigoElemento = this.generaCodigoElemento();
+            ElementoBebida elementoBebida = new ElementoBebida(codigoElemento,
+                    listaBebidas, (SeccionBebida)seccion, nombre, descripcion,foto, precio, divisionesMaximas);
+            elemento.add(elementoBebida);
+
+            // Acceso a BD para insertar el elemento
+    }
 
     public void nuevoElementoPlato(ArrayList<Ingrediente> listaIngredientes,
-            String seccion, String nombre, String descripcion, float precio,
-            ImageIcon foto, float tiempoElaboracion, int divisionesMaximas) {}
+            Seccion seccion, String nombre, String descripcion, float precio,
+            ImageIcon foto, int tiempoElaboracion, int divisionesMaximas) {
+
+            // Comprobación que el elemento es correcto
+            int codigoElemento = this.generaCodigoElemento();
+            ElementoPlato elementoPlato = new ElementoPlato(codigoElemento,
+                    listaIngredientes, (SeccionComida)seccion, nombre, descripcion, foto,
+                    tiempoElaboracion, precio, divisionesMaximas);
+            elemento.add(elementoPlato);
+
+            // Acceso a BD para insertar elemento
+    }
 
     public ArrayList<Elemento> obtenElementosDeSeccion(Seccion seccion) {
         return new ArrayList<Elemento>();
@@ -86,6 +128,5 @@ public class GestorCarta implements IPreparaCarta, ICarta {
     public ArrayList<Seccion> obtenSecciones() {
         return new ArrayList<Seccion>();
     }
-
 
 }
