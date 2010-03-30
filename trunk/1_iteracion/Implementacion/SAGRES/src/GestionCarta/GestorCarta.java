74bf8f3496feb99b0e5792144c175f3362a3de0b
carlos.salas@gmail.com <carlos.salas@gmail.com@841c7c5c-26d4-11df-9191-49dca8b91aeb>
2010-03-30 09:31:59 +0000

    Añadidos métodos que faltaban de la nueva revisión del diseño. Corregido nombre en variables.

--- trunk/1_iteracion/Implementacion/SAGRES/src/GestionCarta/GestorCarta.java
+++ trunk/1_iteracion/Implementacion/SAGRES/src/GestionCarta/GestorCarta.java
@@ -22,8 +22,22 @@ public class GestorCarta implements IPreparaCarta, ICarta {
         this.carta = new Carta();
     }
 
+    private ArrayList<Elemento>buscaElemento (int codigoElemento) {
+        return new ArrayList<Elemento>();
+    }
     private ArrayList<Elemento> buscaElementosInvalidados() {
         return new ArrayList<Elemento>();
+    }
+
+    private boolean compruebaDatosBebida(String nombre, String descripcion,
+            float precio, ImageIcon foto, int divisionesMaximas) {
+            return true;
+    }
+
+    private boolean compruebaDatosPlato(String nombre, String descripcion,
+            float precio, float tiempoElaboracion, ImageIcon foto,
+            int divisionesMaximas) {
+            return true;
     }
 
     public ArrayList<Elemento> corrigeElementosInvalidados() {
@@ -32,24 +46,28 @@ public class GestorCarta implements IPreparaCarta, ICarta {
 
     public void eliminaElementoCarta(int codigoElemento) {}
 
+    public int generaCodigoElemento() {
+        return 1;
+    }
+
     public ArrayList<Elemento> invalidaElementoCarta(Producto producto) {
         return new ArrayList<Elemento>();
     }
 
     public void modificaElementoBebida(int codigoElemento, String nombre,
-            String descripcion, ImageIcon foto, float precio, float diviMax ) {}
+            String descripcion, ImageIcon foto, float precio, float divisionesMaximas ) {}
 
     public void modificaElementoPlato(int codigoElemento, String nombre,
             String descripcion, ImageIcon foto, float tiempoPreparacion,
-            float precio, int diviMax) {}
+            float precio, int divisionesMaximas) {}
 
     public void nuevoElementoBebida(ArrayList<Bebida> listaBebidas,
             String seccion, String nombre, String descripcion, float precio,
-            ImageIcon foto, int diviMax) {}
+            ImageIcon foto, int divisionesMaximas) {}
 
     public void nuevoElementoPlato(ArrayList<Ingrediente> listaIngredientes,
             String seccion, String nombre, String descripcion, float precio,
-            ImageIcon foto, float tiempoElaboracion, int diviMax) {}
+            ImageIcon foto, float tiempoElaboracion, int divisionesMaximas) {}
 
     public ArrayList<Elemento> obtenElementosDeSeccion(Seccion seccion) {
         return new ArrayList<Elemento>();
--- trunk/1_iteracion/Implementacion/SAGRES/src/GestionCarta/IPreparaCarta.java
+++ trunk/1_iteracion/Implementacion/SAGRES/src/GestionCarta/IPreparaCarta.java
@@ -15,18 +15,18 @@ public interface IPreparaCarta {
     public void eliminaElementoCarta(int codigoElemento);
 
     public void modificaElementoBebida(int codigoElemento, String nombre,
-            String descripcion, ImageIcon foto, float precio, float diviMax );
+            String descripcion, ImageIcon foto, float precio, float divisionesMaximas );
 
     public void modificaElementoPlato(int codigoElemento, String nombre,
             String descripcion, ImageIcon foto, float tiempoPreparacion,
-            float precio, int diviMax);
+            float precio, int divisionesMaximas);
 
     public void nuevoElementoBebida(ArrayList<Bebida> listaBebidas,
             String seccion, String nombre, String descripcion, float precio,
-            ImageIcon foto, int diviMax);
+            ImageIcon foto, int divisionesMaximas);
 
     public void nuevoElementoPlato(ArrayList<Ingrediente> listaIngredientes,
             String seccion, String nombre, String descripcion, float precio,
-            ImageIcon foto, float tiempoElaboracion, int diviMax);
+            ImageIcon foto, float tiempoElaboracion, int divisionesMaximas);
 
 }
