424dc8335d91e92c994577d8a88d8c4ac2e26373
elpilli <elpilli@gmail.com>
2010-03-31 13:53:07 +0000

    Primera version de la implementacion del interfaz para el almacenamiento en Base de Datos. La ha añadido la clase Imagen que contiene utilidades para el manejo de imagenes.

--- 1_iteracion/Implementacion/SAGRES/src/GestionBaseDatos/IAlmacenamiento.java
+++ 1_iteracion/Implementacion/SAGRES/src/GestionBaseDatos/IAlmacenamiento.java
@@ -3,12 +3,14 @@ package GestionBaseDatos;
 import javax.swing.table.TableModel;
 
 /**
+ * Interfaz de comunicación con la Base de Datos.
  *
- * @author Daniel
+ * @author Daniel, Ángel Luis García
  */
 public interface IAlmacenamiento {
 
     public abstract boolean consultaDeModificacion(String SQL);
+    public abstract boolean consultaDeModificacionBlob(String SQL, byte[] bytes);
     public abstract TableModel realizaConsulta( String SQL );
 
 }
