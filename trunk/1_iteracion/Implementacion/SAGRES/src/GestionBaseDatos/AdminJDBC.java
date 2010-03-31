690e99f6c6f2757cadd04bc898b54e7b29b9c007
elpilli@gmail.com <elpilli@gmail.com@841c7c5c-26d4-11df-9191-49dca8b91aeb>
2010-03-31 13:53:07 +0000

    Primera version de la implementacion del interfaz para el almacenamiento en Base de Datos. La ha añadido la clase Imagen que contiene utilidades para el manejo de imagenes.

---
+++ trunk/1_iteracion/Implementacion/SAGRES/src/GestionBaseDatos/AdminJDBC.java
@@ -0,0 +1,139 @@
+package GestionBaseDatos;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.ImageIcon;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Clase encargada de implementar el interfaz de comunicación con la Base de Datos.
+ *
+ * @author Daniel, Ángel Luis García
+ */
+public class AdminJDBC implements IAlmacenamiento{
+    private Connection conexion;
+    private String host;
+    private String user;
+    private String pass;
+
+
+     /**
+      * Construye una instacian para conectar y realizar transacciones con la base de datos.
+      *
+      * @param host La dirección del host, con el formato: Dirección/nombreBaseDatos
+      *        Ejemplo: dirección = "localhost", BaseDatos = "Touchteam"
+      *                 Host = "localhost/Toucteam"
+      * @param usuario Nombre del usuario con permisos para acceder a la base de datos.
+      * @param contrasena Contraseña asociada al usuario anteriormente especificado.
+      *
+      */
+    public AdminJDBC(String host,String usuario, String contrasena){
+        this.host = "jdbc:mysql://"+host;
+        this.user = usuario;
+        this.pass = contrasena;
+    }
+
+     /**
+      * Inicia el proceso de conexión
+      *
+      * @throws Si tiene problemas con la conexion lo indicara con un objeto exception que incluira un mensaje
+      * con la descripción del problema.
+      */
+    public void conecta() throws Exception{
+        try {
+            conexion = DriverManager.getConnection(this.host, this.user, this.pass);
+        } catch (SQLException ex) {
+            switch(ex.getErrorCode()){
+                case 1045 :
+                    throw new Exception("Error de Conexion: Usuario o contraseña incorrecta");
+                case 1049 :
+                    throw new Exception("Error de Conexion: El host especificado no es correcto o la base de datos no esta");
+                default:
+                    throw ex;
+            }
+        }
+    }
+
+    /**
+    * Indica Si la instancia esta conectada con la base de datos.
+    *
+    * @return devuelve True si esta conectada, y false en caso contrario
+    */
+    public boolean estaConectado() {
+        try {
+            return this.conexion.isValid(3);
+        } catch (SQLException ex) {
+            return false;
+        }
+    }
+
+     /**
+      * Esta Operación nos permite ejecutar CONSULTAS SQL contra la base de datos.
+      * No olvidemos que para realizar la consulta debe haberse conectado antes con la base de datos.
+      *
+      */
+    public TableModel realizaConsulta( String SQL ){
+        Statement consulta;//Objeto para las consultas
+        ResultSet resultado;//Objeto para los resultados
+        DefaultTableModel modelo = null;
+        try{
+            consulta = (Statement) this.conexion.createStatement(); // creamos una consulta asociada a la conexion
+            resultado = consulta.executeQuery(SQL);// Ejecutamos la consulta
+            modelo = new DefaultTableModel(); //Creamos la estructura para guardar los datos(UNA TABLA)
+            for (int i = 1; i <= resultado.getMetaData().getColumnCount(); i++) {//Relleneamos la cabecera de la tabla.
+                modelo.addColumn(resultado.getMetaData().getColumnName(i));
+            }
+            while (resultado.next()){//introducimos los datos en la Tabla
+                Object [] tupla = new Object[resultado.getMetaData().getColumnCount()];
+                for (int i=0; i<resultado.getMetaData().getColumnCount();i++)
+                    tupla[i] = resultado.getObject(i+1);
+                modelo.addRow(tupla);
+            }
+            return modelo;
+        }catch (SQLException ex){
+            System.err.println("Error: " + ex.getMessage());
+            return modelo;
+        }
+        
+    }
+
+    public boolean consultaDeModificacion(String SQL){
+        Statement consulta;
+        try{
+            consulta = (Statement) this.conexion.createStatement();
+            consulta.executeQuery(SQL);
+            return true;
+        }catch (SQLException ex){
+            System.err.println("Error: " + ex.getMessage());
+            return false;
+        }
+    }
+
+
+    public boolean consultaDeModificacionBlob(String SQL, byte[] bytes){
+        PreparedStatement consulta;
+        ImageIcon im;
+
+        try{
+            consulta = (PreparedStatement)this.conexion.prepareStatement(SQL);
+            consulta.setBytes(1, bytes);
+            consulta.executeUpdate();
+
+            return true;
+        }catch (SQLException ex){
+            System.err.println("Error: " + ex.getMessage());
+            return false;
+        }
+    }
+}
---
+++ trunk/1_iteracion/Implementacion/SAGRES/src/java/util/Imagen.java
@@ -0,0 +1,61 @@
+package java.util;
+
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+/**
+ * Esta clase posee médotos utiles para trabajar con las imágenes.
+ * 
+ * @author angel
+ */
+public class Imagen {
+
+    /**
+     * Método de clase para transformar un ImageIcon en un array de bytes.
+     *
+     * @param imagen La imagen a transformar.
+     * @return Array de bytes conteniendo la imagen.
+     */
+    public static byte[] imageIconToByteArray(ImageIcon imagen) {
+        BufferedImage bi=new BufferedImage(imagen.getImage().getWidth(null),imagen.getImage().getHeight(null), BufferedImage.TYPE_INT_RGB);
+        Graphics g = bi.createGraphics();
+
+        g.drawImage(imagen.getImage(), 0, 0, null);
+        g.dispose();
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(bi, "jpeg", stream);
+        } catch (IOException ex) {
+            System.err.println("Error: " + ex.getMessage());
+        }
+        return stream.toByteArray();
+    }
+
+    /**
+     * Método de clase para transformar un Blob en un ImageIcon.
+     *
+     * @param blob El Blob a transformar.
+     * @return Un ImageIcon obtenido a partir de los datos del Blob.
+     */
+    public static ImageIcon blobToImageIcon(Blob blob){
+
+        ImageIcon imagen = null;
+        try{
+            imagen = new ImageIcon(blob.getBytes(1, (int) blob.length()));
+            return imagen;
+        } catch (SQLException ex) {
+            System.err.println("Error: " + ex.getMessage());
+            return imagen;
+        }
+    }
+}
