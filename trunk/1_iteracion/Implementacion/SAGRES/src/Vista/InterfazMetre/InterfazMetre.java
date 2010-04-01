eb60b9c84e85f1eedcf71bc0f6cce21492b62718
danigrmartinez <danigrmartinez@841c7c5c-26d4-11df-9191-49dca8b91aeb>
2010-04-01 12:51:21 +0000

    InterfazMetre termianda (falta funcionalidad)

---
+++ trunk/1_iteracion/Implementacion/SAGRES/src/Vista/InterfazMetre/InterfazMetre.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * InterfazMetre.java
+ *
+ * Created on 01-abr-2010, 13:32:37
+ */
+
+package Vista.InterfazMetre;
+
+import GestionStock.GestionIncidencias.IIncidencia;
+import GestionStock.GestionProductos.IGestionarProducto;
+import GestionStock.GestionProductos.IProducto;
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Daniel
+ */
+public class InterfazMetre extends javax.swing.JFrame {
+
+    private IIncidencia iIncidencia;
+    private IProducto iProducto;
+    private IGestionarProducto iGestorProducto;
+    private IntPrincipalMetre panelPrincipal = null;
+
+
+    public InterfazMetre(IIncidencia iIncidencia, IProducto iProducto, IGestionarProducto iGestorProducto) {
+        initComponents();
+        this.iIncidencia = iIncidencia;
+        this.iProducto = iProducto;
+        this.iGestorProducto = iGestorProducto;
+        this.panelPrincipal = new IntPrincipalMetre();
+        getContentPane().add(this.panelPrincipal,java.awt.BorderLayout.CENTER);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.panelPrincipal.bSalir.addActionListener(new ManejaEventos(this, ManejaEventos.SALIR));
+        this.panelPrincipal.bAnadirBebida.addActionListener(new ManejaEventos(this, ManejaEventos.ANADIRBEBIDA));
+        this.panelPrincipal.bEliminarBebida.addActionListener(new ManejaEventos(this, ManejaEventos.ELIMNARBEBIDA));
+        this.panelPrincipal.bModificarBebida.addActionListener(new ManejaEventos(this, ManejaEventos.MODIFICARBEBIDA));
+        this.panelPrincipal.bNotificcarIncidencia.addActionListener(new ManejaEventos(this, ManejaEventos.NOTIFICARINCIDENCIA));
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new InterfazMetre(null,null,null).setVisible(true);
+            }
+        });
+    }
+
+    private class ManejaEventos implements ActionListener {
+
+        private int tipoEvento;
+        private JFrame padre;
+        protected static final int SALIR = 0;
+        protected static final int ANADIRBEBIDA = 1;
+        protected static final int ELIMNARBEBIDA = 2;
+        protected static final int MODIFICARBEBIDA = 3;
+        protected static final int NOTIFICARINCIDENCIA = 4;
+
+        public ManejaEventos(JFrame Padre, int TipoEvento){
+            this.padre = Padre;
+            this.tipoEvento = TipoEvento;
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            Dialog dialogo= null;
+            switch (this.tipoEvento) {
+                case ManejaEventos.SALIR:
+                    System.exit(0);
+                    break;
+                case ManejaEventos.ANADIRBEBIDA:
+                    dialogo = new DialogoAnadirBebida(padre, true);
+                    break;
+                case ManejaEventos.ELIMNARBEBIDA:
+                    dialogo =new DialogoEliminarBebida(padre, true);
+                    break;
+                case ManejaEventos.MODIFICARBEBIDA:
+                    dialogo =new DialogoModificarBedidas(padre, true);
+                    break;
+                case ManejaEventos.NOTIFICARINCIDENCIA:
+                    dialogo =new DialogoNotificarIncidenciaBebida(padre, true);
+                    break;
+            }
+            if(dialogo != null){
+                dialogo.setLocationRelativeTo(padre);
+                dialogo.setVisible(true);
+            }
+        }
+
+    }
+
+
+
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
+}
