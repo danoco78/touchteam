014510dfd9d03f19c6e6711387ffd4fb5c0baaab
danigrmartinez <danigrmartinez@841c7c5c-26d4-11df-9191-49dca8b91aeb>
2010-04-02 16:28:47 +0000

    Arreglado problemas en el paquete de incidencias

--- 1_iteracion/Implementacion/SAGRES/src/GestionStock/GestionIncidencias/GestorIncidencia.java
+++ 1_iteracion/Implementacion/SAGRES/src/GestionStock/GestionIncidencias/GestorIncidencia.java
@@ -11,6 +11,7 @@ import GestionStock.GestionProductos.IGestionarProducto;
 import GestionStock.GestionProductos.IProducto;
 import GestionStock.GestionProductos.Producto;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import javax.swing.table.TableModel;
 
@@ -26,6 +27,19 @@ public class GestorIncidencia implements IIncidencia {
     IProducto almacenProductos;
     ArrayList<Incidencia> incidencias;
 
+    private static final String ULTIMOID =
+            "select MAX(incidencia_id) from incidencia;";
+    private static final String TABLAINCIDENCIAS =
+            "select incidencia_id, descripcion, fecha, cantidad_afectada, producto_producto_id " +
+            " from incidencia , tieneIncidencia " +
+            " where incidencia_id = incidencia_incidencia_id";
+    private static final String INI_INSERTAR_INCIDNECIA =
+            "insert into incidencia(descripcion,fecha,cantidad_afectada) values (";
+            /*"Caida",10/5/2011,10*/
+    private static final String FIN_INSERTAR = ");";
+    private static final String INI_INSERTAR_RELACION =
+            "insert into tieneincidencia(incidencia_incidencia_id,producto_producto_id) values (";
+
 
     public GestorIncidencia( IGestionarProducto iGestorProductos, ICarta iCarta,
             IAlmacenamiento iAlmacenamiento, IProducto iProducto ){
@@ -33,33 +47,40 @@ public class GestorIncidencia implements IIncidencia {
         this.carta = iCarta;
         this.almacen = iAlmacenamiento;
         this.almacenProductos = iProducto;
-        TableModel datos = this.almacen.realizaConsulta("select incidencia_id, descripccion, fecha," +
-                " cantidad_afectada, porducto_id from incidencia , tieneIncidencia  " +
-                "where incidencia_id = incidencia_incidencia_id");
-        ArrayList<Producto> listaProductos = this.almacenProductos.obtenerListaProductos();
-        for (int i = 0; i < datos.getRowCount(); i++) {
-            Producto producto = null;
-            for (int j = 0; j < listaProductos.size(); j++) {
-                if( listaProductos.get(j).getCodPro() == datos.getValueAt(i, 4) )
-                    producto = listaProductos.get(j);
-            }
-            Incidencia incidencia = new Incidencia((Integer)datos.getValueAt(i, 0), producto ,
-                    (Float)datos.getValueAt(i, 3),(String)datos.getValueAt(i, 1),(Date)datos.getValueAt(i, 2));
-            datos.getValueAt(i, 0);
-        }
+        TableModel datos = this.almacen.realizaConsulta(GestorIncidencia.TABLAINCIDENCIAS);
+        this.incidencias = this.convertirTablaAIncidencia(datos);
     }
     
 
     public void nuevaIncidencia(String tipoIncidencia, float cantidadAfectada, Producto producto) throws Exception {
-            int codigo = this.generarCodigoIncidencia();
-            Incidencia incidencia = new Incidencia(codigo, producto, cantidadAfectada, tipoIncidencia);
-            this.gestorProductos.actualizaCantidadProducto(producto,-cantidadAfectada);
-            this.almacen.consultaDeModificacion("insert ....");
+            Calendar c = Calendar.getInstance();
+            this.almacen.consultaDeModificacion(GestorIncidencia.INI_INSERTAR_INCIDNECIA
+                    +tipoIncidencia+", '"
+                    +c.get(Calendar.DAY_OF_MONTH)+"/"+c.get(Calendar.MONTH)+"/"+c.get(Calendar.MONTH)+"',"+
+                    cantidadAfectada + GestorIncidencia.FIN_INSERTAR);
+            TableModel datos = this.almacen.realizaConsulta(GestorIncidencia.ULTIMOID);
+            Incidencia incidencia = new Incidencia((Integer)datos.getValueAt(0,0), producto,
+                    cantidadAfectada, tipoIncidencia, null);
             this.incidencias.add(incidencia);
+            this.almacen.consultaDeModificacion( GestorIncidencia.INI_INSERTAR_RELACION+
+                    (Integer)datos.getValueAt(0,0)+", "+producto.getCodPro()+GestorIncidencia.FIN_INSERTAR );
     }
 
-    private int generarCodigoIncidencia(){
-        return 0;
+    private ArrayList<Incidencia> convertirTablaAIncidencia(TableModel tabla){
+        ArrayList<Producto> listaProductos = this.almacenProductos.obtenerListaProductos();
+        ArrayList<Incidencia> almacenIncidencias = new ArrayList<Incidencia>();
+        for (int i = 0; i < tabla.getRowCount(); i++) {
+            Producto producto = null;
+            for (int j = 0; j < listaProductos.size(); j++) {
+                if (listaProductos.get(j).getCodPro() == tabla.getValueAt(i, 4)) {
+                    producto = listaProductos.get(j);
+                }
+            }
+            Incidencia incidencia = new Incidencia((Integer) tabla.getValueAt(i, 0), producto,
+                    (Float) tabla.getValueAt(i, 3), (String) tabla.getValueAt(i, 1), (Date) tabla.getValueAt(i, 2));
+            almacenIncidencias.add(incidencia);
+        }
+        return almacenIncidencias;
     }
 
 }
