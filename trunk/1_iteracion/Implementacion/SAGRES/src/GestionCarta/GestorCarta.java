2afc4ec5b79c54062f168b080dd2cce88820a6b9
carlos.salas@gmail.com <carlos.salas@gmail.com@841c7c5c-26d4-11df-9191-49dca8b91aeb>
2010-04-05 10:34:04 +0000

    Modificación en constructor y funciones convertirTabla_A_X() en GestorCarta

--- trunk/1_iteracion/Implementacion/SAGRES/src/GestionCarta/GestorCarta.java
+++ trunk/1_iteracion/Implementacion/SAGRES/src/GestionCarta/GestorCarta.java
@@ -21,6 +21,8 @@ import javax.swing.table.TableModel;
 public class GestorCarta implements IPreparaCarta, ICarta {
 
     ArrayList<Elemento> listaElementos;
+    ArrayList<ElementoBebida> listaElementosBebida;
+    ArrayList<ElementoPlato> listaElementosPlato;
     Carta carta;
     ArrayList<Seccion> listaSecciones;
     IAlmacenamiento almacen;
@@ -45,9 +47,17 @@ public class GestorCarta implements IPreparaCarta, ICarta {
         tabla = this.almacen.realizaConsulta("SELECT elemento_id, descripcion, divi_max, foto, nombre, precio FROM elemento");
         for (int i=0;i<tabla.getRowCount();i++) {
             Elemento elemento = new Elemento((Integer)tabla.getValueAt(i,0), (String)tabla.getValueAt(i, 1), (Integer)tabla.getValueAt(i, 2),
-                    (ImageIcon)Imagen.blobToImageIcon((Blob)tabla.getValueAt(i, 3)), (String)tabla.getValueAt(i, 4), (Float)tabla.getValueAt(i, 5));
+                 (ImageIcon)Imagen.blobToImageIcon((Blob)tabla.getValueAt(i, 3)), (String)tabla.getValueAt(i, 4), (Float)tabla.getValueAt(i, 5));
             this.listaElementos.add(elemento);
         }
+
+        // Construimos la lista de ElementoBebida
+        tabla = this.almacen.realizaConsulta("SELECT elemento_elemento_id FROM elementobebida");
+        this.listaElementosBebida = this.convierteTablaABebida(tabla);
+
+        // Construimos la lista de ElementoPlato
+        tabla = this.almacen.realizaConsulta("SELECT elemento_elemento_id, tiempo_elaboracion FROM elementoplato");
+        this.listaElementosPlato = this.convierteTablaAPlato(tabla);
         
     }
 
@@ -79,56 +89,70 @@ public class GestorCarta implements IPreparaCarta, ICarta {
      * @param tabla Tabla con los datos de la BD
      * @return Un ElementoBebida
      */
-    private ElementoBebida convierteTablaABebida(TableModel tabla) {
-        TableModel tablaBebida;
+    private ArrayList<ElementoBebida> convierteTablaABebida(TableModel tabla) {
+        TableModel tablaBebida, tablaDatosElemento;
         String consulta;
         ArrayList<Bebida> listaBebidas = new ArrayList<Bebida>();
+        ArrayList listaElementosB = new ArrayList<ElementoBebida>();
         ElementoBebida elementoBebida;
-        // Obtenemos de la BD todas las bebidas del elemento
-        consulta = "SELECT producto.producto_id, producto.nombre, producto.foto, producto.minimo, producto.maximo, producto.cantidad FROM tieneBebida, producto WHERE" +
-                " tieneBebida.productoBebida_producto_producto_id" +
-                " = producto.producto_id AND tieneBebida.elementoBebida_elemento_elemento_id ='"+tabla.getValueAt(0,0)+"')";
-        tablaBebida = almacen.realizaConsulta(consulta);
-        // Para cada bebida obtenida de la BD, creamos su objeto e insertamos en multiobjeto
-        for (int i=0;i<tablaBebida.getRowCount();i++) {
-            Bebida bebida = new Bebida((Integer)tablaBebida.getValueAt(i, 0),(String)tablaBebida.getValueAt(i, 1),
-                    (ImageIcon)Imagen.blobToImageIcon((Blob)tablaBebida.getValueAt(i, 2)),(Float)tablaBebida.getValueAt(i, 3),
-                    (Float)tablaBebida.getValueAt(i, 4), (Float)tablaBebida.getValueAt(i, 5));
-            listaBebidas.add(bebida);
+        for (int i=0;i<tabla.getRowCount();i++) {
+            // Obtenemos de la BD todas las bebidas del elemento
+            consulta = "SELECT producto.producto_id, producto.nombre, producto.foto, producto.minimo, producto.maximo, producto.cantidad FROM tieneBebida, producto WHERE" +
+                    " tieneBebida.productoBebida_producto_producto_id" +
+                    " = producto.producto_id AND tieneBebida.elementoBebida_elemento_elemento_id ='"+tabla.getValueAt(i,0)+"')";
+            tablaBebida = almacen.realizaConsulta(consulta);
+            // Para cada bebida obtenida de la BD, creamos su objeto e insertamos en multiobjeto
+            for (int j=0;j<tablaBebida.getRowCount();j++) {
+                Bebida bebida = new Bebida((Integer)tablaBebida.getValueAt(j, 0),(String)tablaBebida.getValueAt(j, 1),
+                        (ImageIcon)Imagen.blobToImageIcon((Blob)tablaBebida.getValueAt(j, 2)),(Float)tablaBebida.getValueAt(j, 3),
+                        (Float)tablaBebida.getValueAt(j, 4), (Float)tablaBebida.getValueAt(j, 5));
+                listaBebidas.add(bebida);
+            }
+            // Obtenemos los datos del elementoBebida e insertamos en la lista de ElementoBebida
+            consulta = "SELECT elemento_id, nombre, descripcion, foto, precio, divi_max FROM elemento WHERE elemento_id='"+tabla.getValueAt(i,0)+"'";
+            tablaDatosElemento = almacen.realizaConsulta(consulta);
+            elementoBebida = new ElementoBebida((Integer)tablaDatosElemento.getValueAt(0,0), listaBebidas, (String)tablaDatosElemento.getValueAt(0,1),
+                    (String)tablaDatosElemento.getValueAt(0,2), (ImageIcon)Imagen.blobToImageIcon((Blob)tablaDatosElemento.getValueAt(0,3)),
+                    (Float)tablaDatosElemento.getValueAt(0,4),(Integer)tablaDatosElemento.getValueAt(0,5));
+            listaElementosB.add(elementoBebida);
         }
-        elementoBebida = new ElementoBebida((Integer)tabla.getValueAt(0,0), listaBebidas, (String)tabla.getValueAt(0,1),
-                (String)tabla.getValueAt(0,2), (ImageIcon)Imagen.blobToImageIcon((Blob)tabla.getValueAt(0,3)), (Float)tabla.getValueAt(0,4),
-                (Integer)tabla.getValueAt(0,5));
         
-        return elementoBebida;
+        return listaElementosB;
     }
     /*
      * Funcion que convierte una tabla en un objeto ElementoPlato
      * @param tabla Tabla con los datos de la BD
      * @return Un ElementoPlato
      */
-    private ElementoPlato convierteTablaAPlato(TableModel tabla) {
-        TableModel tablaPlato;
+    private ArrayList<ElementoPlato> convierteTablaAPlato(TableModel tabla) {
+        TableModel tablaPlato, tablaDatosElemento;
         String consulta;
         ArrayList<Ingrediente> listaIngredientes = new ArrayList<Ingrediente>();
+        ArrayList<ElementoPlato> listaElementoP = new ArrayList<ElementoPlato>();
         ElementoPlato elementoPlato;
-        // Obtenemos de la BD todos los ingredientes del elemento
-        consulta = "SELECT producto.producto_id, producto.nombre, producto.cantidad, producto.maximo, producto.minimo, producto.foto FROM tieneIngrediente, producto WHERE " +
-                "tieneIngrediente.productoIngrediente_producto_producto_id" +
-                " = producto.producto_id AND tieneIngrediente.elementoIngrediente_elemento_elemento_id ='"+tabla.getValueAt(0,0)+"')";
-        tablaPlato = almacen.realizaConsulta(consulta);
-        // Para cada ingrediente obtenido de la BD, creamos el objeto e insertamos en multiobjeto
-        for (int i=0;i<tablaPlato.getRowCount();i++) {
-            Ingrediente ingrediente = new Ingrediente((Integer)tablaPlato.getValueAt(i, 0),(String)tablaPlato.getValueAt(i, 1),
-                    (Float)tablaPlato.getValueAt(i, 2),(Float)tablaPlato.getValueAt(i, 3),
-                    (Float)tablaPlato.getValueAt(i, 5), (ImageIcon)Imagen.blobToImageIcon((Blob)tablaPlato.getValueAt(i, 5)));
-            listaIngredientes.add(ingrediente);
+        for (int i=0;i<tabla.getRowCount();i++) {
+            // Obtenemos de la BD todos los ingredientes del elemento
+            consulta = "SELECT producto.producto_id, producto.nombre, producto.cantidad, producto.maximo, producto.minimo, producto.foto FROM tieneIngrediente, producto WHERE " +
+                    "tieneIngrediente.productoIngrediente_producto_producto_id" +
+                    " = producto.producto_id AND tieneIngrediente.elementoComida_elemento_elemento_id ='"+tabla.getValueAt(i,0)+"')";
+            tablaPlato = almacen.realizaConsulta(consulta);
+            // Para cada ingrediente obtenido de la BD, creamos el objeto e insertamos en multiobjeto
+            for (int j=0;j<tablaPlato.getRowCount();j++) {
+                Ingrediente ingrediente = new Ingrediente((Integer)tablaPlato.getValueAt(j, 0),(String)tablaPlato.getValueAt(j, 1),
+                        (Float)tablaPlato.getValueAt(j, 2),(Float)tablaPlato.getValueAt(j, 3),
+                        (Float)tablaPlato.getValueAt(j, 5), (ImageIcon)Imagen.blobToImageIcon((Blob)tablaPlato.getValueAt(j, 5)));
+                listaIngredientes.add(ingrediente);
+            }
+            // Obtenemos los datos del elemento
+            consulta = "SELECT elemento_id, nombre, descripcion, foto, precio, divi_max FROM elemento WHERE elemento_id='"+tabla.getValueAt(i,0)+"'";
+            tablaDatosElemento = almacen.realizaConsulta(consulta);
+            // Creamos el objeto elementoPlato e insertamos en la lista de ElementoPlato
+            elementoPlato = new ElementoPlato((Integer)tablaDatosElemento.getValueAt(0,0), listaIngredientes, (String)tablaDatosElemento.getValueAt(0,1),
+                    (String)tablaDatosElemento.getValueAt(0,2), (ImageIcon)Imagen.blobToImageIcon((Blob)tablaDatosElemento.getValueAt(0,3)),
+                    (Integer)tabla.getValueAt(i,1),(Float)tablaDatosElemento.getValueAt(0,5),(Integer)tablaDatosElemento.getValueAt(0,6));
+            listaElementoP.add(elementoPlato);
         }
-        elementoPlato = new ElementoPlato((Integer)tabla.getValueAt(0,0), listaIngredientes, (String)tabla.getValueAt(0,1),
-                (String)tabla.getValueAt(0,2), (ImageIcon)Imagen.blobToImageIcon((Blob)tabla.getValueAt(0,3)), (Integer)tabla.getValueAt(0,4) ,
-                (Float)tabla.getValueAt(0,5),(Integer)tabla.getValueAt(0,6));
-
-        return elementoPlato;
+        return listaElementoP;
     }
 
     public ArrayList<Elemento> corrigeElementosInvalidados() {
@@ -212,7 +236,8 @@ public class GestorCarta implements IPreparaCarta, ICarta {
             int id_elemento = (Integer)tabla.getValueAt(0,0);
             ElementoBebida elementoBebida = new ElementoBebida(id_elemento, listaBebidas, nombre, descripcion, foto, precio, divisionesMaximas);
             seccion.anadeElemento(elementoBebida);
-            listaElementos.add(elementoBebida);
+            this.listaElementos.add(elementoBebida); // Quitarlo si se elimina
+            this.listaElementosBebida.add(elementoBebida);
             consulta = "INSERT INTO elementoBebida VALUES("+id_elemento+")";
             almacen.consultaDeModificacion(consulta);
             // Para cada Bebida, sacamos su idBebida e insertamos junto con idElemento en tieneBebida
@@ -262,7 +287,8 @@ public class GestorCarta implements IPreparaCarta, ICarta {
             int id_elemento = (Integer)tabla.getValueAt(0,0);
             ElementoPlato elementoPlato = new ElementoPlato(id_elemento, listaIngredientes, nombre, descripcion, foto, tiempoElaboracion, precio, divisionesMaximas);
             seccion.anadeElemento(elementoPlato);
-            listaElementos.add(elementoPlato);
+            this.listaElementos.add(elementoPlato); // Quitarlo si se elimina
+            this.listaElementosPlato.add(elementoPlato);
             consulta = "INSERT INTO elementoPlato VALUES('"+id_elemento+"','"+tiempoElaboracion+"')";
             almacen.consultaDeModificacion(consulta);
             // Para cada Ingrediente, sacamos su idIngrediente e insertamos junto con idElemento en tieneIngrediente
