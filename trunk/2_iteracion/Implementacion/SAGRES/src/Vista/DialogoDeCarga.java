8bc5a1379b97540aa0031bd1147c32b4ad56251a
lumley256@gmail.com <lumley256@gmail.com@841c7c5c-26d4-11df-9191-49dca8b91aeb>
2010-05-04 13:49:01 +0000

    Añadida implementacion 2º iteracion

---
+++ trunk/2_iteracion/Implementacion/SAGRES/src/Vista/DialogoDeCarga.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * DialogoDeCarga.java
+ *
+ * Created on 05-abr-2010, 17:54:57
+ */
+
+package Vista;
+
+/**
+ *
+ * @author Daniel
+ */
+public class DialogoDeCarga extends javax.swing.JFrame {
+
+    /** Creates new form DialogoDeCarga */
+    public DialogoDeCarga() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        Progreso = new javax.swing.JProgressBar();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        setUndecorated(true);
+
+        Progreso.setPreferredSize(new java.awt.Dimension(146, 30));
+        getContentPane().add(Progreso, java.awt.BorderLayout.PAGE_END);
+
+        jPanel1.setLayout(new java.awt.GridBagLayout());
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Vista/LogoSagres.png"))); // NOI18N
+        jPanel1.add(jLabel1, new java.awt.GridBagConstraints());
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public void Progreso(int porcent){
+        this.Progreso.setValue(porcent);
+        this.repaint();
+    }
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JProgressBar Progreso;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+
+}
