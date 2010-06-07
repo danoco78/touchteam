3201c217f0b5bcc63fc1c4e88be175aa3a941ea0
theoriginalpelus@gmail.com <theoriginalpelus@gmail.com@841c7c5c-26d4-11df-9191-49dca8b91aeb>
2010-06-07 14:55:15 +0000

    Actualizada web de SAGRES.

--- trunk/3_iteracion/Implementacion/WebSAGRES/modificaPedido.php
+++ trunk/3_iteracion/Implementacion/WebSAGRES/modificaPedido.php
@@ -31,8 +31,9 @@
             $sagres = new ControladorPrincipal();
             $ip = $_SERVER['REMOTE_ADDR'];
             preg_match_all("/\d+.\d+.(\d+).(\d+)/", $ip, $exp);
-            $valor1 = 0+$exp[1][0];
-            $valor2 = (valor1%10)*1000;
+            $valor1 = 0+$exp[2][0];
+            $valor2 = 0+$exp[1][0];
+            $valor2 = ($valor2%10)*1000;
             $codmesa = 1000+$valor1+$valor2;
             $pedidos = $sagres->getPedidosModificablesMesa($codmesa);
             for($i=0; $i<count($pedidos); $i++) {
